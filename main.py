6d7349be231d0f14be57df2f22b56bd59a0ca594
Niklas Dewes <niklas.dewes@telekom.de>
2023-01-17 15:28:31 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect, escape
-import os
 
-from flask import Flask
+from flask import Flask, escape, redirect, render_template, request, url_for
+from flask_login import LoginManager, current_user, login_user, logout_user
+from flask_login.utils import fresh_login_required, login_required
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import Query, scoped_session, sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-main = Blueprint('main', __name__)
+from sqlalchemy.orm import Query, scoped_session, sessionmaker
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
@@ -28,14 +23,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '\xeaF\xa9\x88\xda\xf6\x82\xf4\xa7=\xd6\xa0\xeb[F\xd1A6G\xe0\xc6W2\xb0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 db_session = scoped_session(sessionmaker(bind=engine))
 db.init_app(app)
-
-from .auth import auth as auth_blueprint
-app.register_blueprint(auth_blueprint)
-
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
 
 class Bestellung(Base):
     __table__ = Base.metadata.tables['BESTELLUNG']
@@ -55,15 +47,23 @@ class Preisklasse(Base):
 class Schueler(Base):
     __table__ = Base.metadata.tables['SCHUELER']
 
-@auth.route('/login')
+@login_manager.user_loader
+def load_user(db_dozent_id):
+    return db_session.query(Dozent.db_dozent_id)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route('/login')
 def login():
     return render_template('login.html')
 
-@auth.route('/signup')
+@app.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@auth.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def signup_post():
     if request.method == 'POST':
         email = request.form['email']
@@ -87,16 +87,19 @@ def signup_post():
         db_session.add(dozenten)
         db_session.commit()
 
-@main.route('/')
+@app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
-@main.route('/bestellungen')
+@app.route('/bestellungen')
+@login_required
 def bestellungen():
     result = db_session.query(Bestellung.db_bestellung_id, Bestellung.db_schueler_id, Bestellung.db_kurs_id, Bestellung.db_bestellstatus, Bestellung.db_bestelldatum)
     return render_template('bestellungen.html', bestellung=result)
 
-@main.route('/bestellungen', methods=('GET', 'POST'))
+@app.route('/bestellungen', methods=('GET', 'POST'))
+@login_required
 def create_bestellungen():
     if request.method == 'POST':
         id = int(request.form['id'])
@@ -116,15 +119,14 @@ def create_bestellungen():
 
     return render_template('bestellungen.html')
 
-
-
-
-@main.route('/schueler')
+@app.route('/schueler')
+@login_required
 def schueler():
     result = db_session.query(Schueler.db_schueler_id, Schueler.db_email, Schueler.db_username, Schueler.db_vorname, Schueler.db_nachname)
     return render_template('schueler.html', schueler=result)
 
-@main.route('/schueler', methods=('GET', 'POST'))
+@app.route('/schueler', methods=('GET', 'POST'))
+@login_required
 def create_schueler():
     if request.method == 'POST':
         id = int(request.form['id'])
@@ -147,16 +149,18 @@ def create_schueler():
     return render_template('schueler.html')
 
 
-@main.route('/dozenten')
+@app.route('/dozenten')
+@login_required
 def dozenten():
     result = db_session.query(Dozent.db_vorname, Dozent.db_nachname, Dozent.db_dozent_id)
     return render_template('dozenten.html', dozenten=result)
 
-@main.route('/kurse')
+@app.route('/kurse')
+@login_required
 def kurse():
     result = db_session.query(Kurs.db_kurs_titel, Kurs.db_kurs_id, Kurs.db_dozent_id)
     return render_template('kurse.html', kurse=result)
 
 
 if __name__ == '__main__':
-    main.run(host='127.0.0.1', port='5000', debug=True)
+    app.run(host='127.0.0.1', port='5000', debug=True)
